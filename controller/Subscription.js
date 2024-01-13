2d7fe8078d5f45edfb761a9ab22799aeedea1b9c
@Faimasoft786 <FatimaSoft786@gmail.com>
2024-01-13 10:01:31 +0500

    Vercel link added

--- controller/Subscription.js
+++ controller/Subscription.js
@@ -1,6 +1,7 @@
 const subscriptin = require("../model/subCategoryModel");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const CheckoutSession = async (req, res) => {
+  console.log(req.body)
   try {
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
@@ -17,12 +18,12 @@ const CheckoutSession = async (req, res) => {
             quantity: item.quantity
           }
         }),
-        success_url: "http://localhost:5000/success",
-        cancel_url: "http://localhost:5000/cancel"
+        success_url: "https://sokhtamon-frontend.vercel.app/",
+        cancel_url: "https://sokhtamon-frontend.vercel.app/cancel"
       });
       res.json({url: session.url})
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error)
   }
 };
 
